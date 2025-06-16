9a2b23cac57276f0c5be1db809c27246b9d7f775
ItzOliee <oliver.123@live.co.uk>
2025-06-16 10:54:14 +0100

    Add files via upload

--- home-manager/modules/scripts/pywaldunst.sh
+++ home-manager/modules/scripts/pywaldunst.sh
@@ -3,9 +3,41 @@
 source ~/.cache/wal/colors.sh
 
 cat <<EOF
+
 [global]
-geometry = "300x50-10+10"
-timeout = 3000
+# Display
+follow = mouse
+width = 350
+height = (0, 300)
+origin = top-right
+offset = (35, 35)
+indicate_hidden = yes
+notification_limit = 5
+gap_size = 12
+padding = 12
+horizontal_padding = 20
+frame_width = 1
+sort = no
+
+# Progress bar
+progress_bar_frame_width = 0
+progress_bar_corner_radius = 3
+
+# Colors
+foreground = "#cdd1dc"
+frame_color = "#2d303c"
+highlight = "#2274d5, #82aad9"
+
+# Text
+font = Noto Sans CJK JP 10
+markup = full
+format = "<small>%a</small>\n<b>%s</b>\n%b"
+alignment = left
+vertical_alignment = center
+show_age_threshold = -1
+hide_duplicate_count = false
+
+
 
 [urgency_low]
 background = "${background:-#282828}"
