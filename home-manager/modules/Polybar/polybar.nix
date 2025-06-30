6bdd29b2278fa2621214f3d0b419d647a737b270
ItzOliee <oliver.123@live.co.uk>
2025-06-30 13:09:47 +0100

    Add files via upload

--- home-manager/modules/Polybar/polybar.nix
+++ home-manager/modules/Polybar/polybar.nix
@@ -1,7 +1,10 @@
-{
+{ 
   services.polybar = {
     enable = true;
     config = ./config.ini;
-    script = ''/home/oli/.config/home-manager/modules/Polybar/launch_polybar.sh'';
-  };
+
+    package = pkgs.polybar.override {
+      i3GapsSupport = true;
+      alsaSupport = true;
+    };
 }
