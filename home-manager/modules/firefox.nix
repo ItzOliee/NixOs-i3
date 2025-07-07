412d48dbe959a3432eecc175eadabad9fd68f44b
ItzOliee <oliver.123@live.co.uk>
2025-07-07 15:21:52 +0100

    Add files via upload

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -13,225 +13,216 @@ in {
     "${chromeDir}/userChrome.css".text = ''
 
 
+    :root {
+  --accent: var(--pywal-color7);
+  --main-bg: var(--pywal-background);
+  --dark-bg: var(--pywal-color0);
+  --light-bg: var(--pywal-color1);
 
-         :root {
-          --accent: white;
-          --main-bg: #141929;
-          --dark-bg: #0d1120;
-          --light-bg: #171c2a;
+  --primary: var(--pywal-color7);
+  --primary-alt: var(--pywal-color6);
+  --primary-grad: linear-gradient(90deg, var(--primary), var(--primary-alt));
 
-          --primary: #39a7fe;
-          --primary-alt: #0675dd;
+  --toolbar-bgcolor: var(--main-bg) !important;
+  --lwt-sidebar-background-color: var(--dark-bg) !important;
+  --lwt-sidebar-text-color: var(--pywal-foreground, rgba(255, 255, 255, 0.5));
+}
 
-          --primary: #dc6971;
-          --primary-alt: #dc6971;
+* {
+  font-family: "Hack Nerd Font" !important;
+  font-size: 14px !important;
+}
 
-          --primary-grad: linear-gradient(90deg, var(--primary), var(--primary-alt));
+element {
+  --lwt-sidebar-text-color: var(--pywal-foreground, rgba(255, 255, 255, 0.5));
+}
 
-          --toolbar-bgcolor: var(--main-bg) !important;
-          --lwt-sidebar-background-color: var(--dark-bg) !important;
-          --lwt-sidebar-text-color: rgba(255, 255, 255, 0.5);
-      }
+#TabsToolbar #firefox-view-button[open] > .toolbarbutton-icon:-moz-lwtheme,
+.tab-background[selected]:-moz-lwtheme {
+  outline: none !important;
+}
 
-      * {
-          font-family: "Hack Nerd Font" !important;
-          font-size: 14px !important;
-      }
+/* Compact bookmarks and folders */
+#folderTree > treechildren::-moz-tree-row,
+#bookmarks-view > treechildren::-moz-tree-row {
+  height: 19px !important;
+  min-height: 19px !important;
+}
 
-      element {
-        --lwt-sidebar-text-color: rgba(255, 255, 255, 0.5);
-      }
+/* Toolbar spacing */
+#nav-bar toolbarspring {
+  min-width: 10px !important;
+  max-width: 20px !important;
+}
 
-      #TabsToolbar #firefox-view-button[open] > .toolbarbutton-icon:-moz-lwtheme, .tab-background[selected]:-moz-lwtheme {
-        outline: none !important;
-      }
+#nav-bar-customization-target {
+  background-color: var(--main-bg) !important;
+  -moz-appearance: none !important;
+  background-image: none !important;
+  border: none !important;
+  color: var(--pywal-foreground) !important;
+}
 
-      #folderTree > treechildren::-moz-tree-row {
-        height: 19px !important;
-        min-height: 19px !important;
-      }
+#main-window,
+#navigator-toolbox,
+#PersonalToolbar,
+#sidebar-box,
+#tabbrowser-tabs,
+#TabsToolBar {
+  background-color: var(--main-bg) !important;
+  -moz-appearance: none !important;
+  background-image: none !important;
+  border: none !important;
+  color: var(--pywal-foreground) !important;
+}
 
-      #bookmarks-view > treechildren::-moz-tree-row {
-        height: 19px !important;
-        min-height: 19px !important;
-      }
+#tabbrowser-tabs {
+  --tab-loading-fill: none !important;
+}
 
-      #nav-bar toolbarspring {
-        min-width: 10px !important;
-        max-width: 20px !important;
-      }
+#TabsToolbar .tabbrowser-tab[selected] {
+  color: var(--main-bg) !important;
+}
 
-      #nav-bar-customization-target {
-        background-color: var(--main-bg) !important;
-        -moz-appearance: none !important;
-        background-image: none !important;
-        border: none !important;
-        color: white !important;
-      }
+#TabsToolbar .tabbrowser-tab {
+  color: rgba(194, 205, 226, 0.918) !important;
+}
 
-      #main-window,
-      #navigator-toolbox,
-      #PersonalToolbar,
-      #sidebar-box,
-      #main-window,
-      #tabbrowser-tabs,
-      #TabsToolBar {
-        background-color: var(--main-bg) !important;
-        -moz-appearance: none !important;
-        background-image: none !important;
-        border: none !important;
-        color: white !important;
-      }
+.tab-content {
+  padding: 14px 20px !important;
+}
 
-      #tabbrowser-tabs {
-          --tab-loading-fill: none !important;
-      }
+.tab-background {
+  background-color: var(--main-bg) !important;
+  margin: 12px !important;
+  margin-right: 0px !important;
+  border-radius: 10px !important;
+  color: var(--pywal-foreground) !important;
+  border: 6px solid var(--main-bg) !important;
+  transition: all .2s !important;
+}
 
-      #TabsToolbar .tabbrowser-tab[selected] {
-          color: var(--main-bg) !important;
-      }
+#tabbrowser-arrowscrollbox .tabbrowser-tab:last-of-type .tab-background,
+#tabs-newtab-button {
+  margin-right: 10px !important;
+}
 
-      #TabsToolbar .tabbrowser-tab {
-          color: rgba(194, 205, 226, 0.918) !important;
-      }
+.tabbrowser-tab:hover .tab-background {
+  background-color: var(--dark-bg) !important;
+  border-color: var(--dark-bg) !important;
+}
 
-      .tab-content {
-        padding: 14px 20px !important;
-      }
+.tab-background[selected="true"] {
+  background: var(--primary-grad) !important;
+  border: 0px solid var(--main-bg) !important;
+  color: var(--main-bg) !important;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, .2);
+}
 
-      .tab-background {
-        background-color: var(--main-bg) !important;
-        margin: 12px !important;
-        margin-right: 0px !important;
-        border-radius: 10px !important;
-        color: white !important;
-        border: 6px solid var(--main-bg) !important;
-        transition: all .2s !important;
-      }
+.tab-line,
+.tabbrowser-tab::before,
+.tabbrowser-tab::after,
+.tab-loading-burst {
+  display: none !important;
+}
 
-      #tabbrowser-arrowscrollbox .tabbrowser-tab:last-of-type .tab-background, #tabs-newtab-button {
-        margin-right: 10px !important;
-      }
+.tab-close-button {
+  border-radius: 10px !important;
+}
 
-      .tabbrowser-tab:hover .tab-background {
-        background-color: var(--dark-bg) !important;
-        border-color: var(--dark-bg ) !important;
-      }
+#identity-box,
+#tracking-protection-icon-container,
+#urlbar-background,
+#whats-new-menu-button,
+#star-button,
+#pocket-button,
+#pageActionSeparator,
+#pageActionButton,
+#reader-mode-button {
+  display: none !important;
+}
 
-      .tab-background[selected="true"] {
-          background: var(--primary-grad) !important;
-          border: 0px solid var(--main-bg) !important;
-          color: var(--main-bg) !important;
-          box-shadow: 1px 1px 10px rgba(0,0,0,.2);
-      }
+#back-button>.toolbarbutton-icon {
+  transform: scale(.85, .85) !important;
+  animation: none !important;
+  border: none !important;
+  box-shadow: none !important;
+}
 
-      .tab-line {
-          display: none !important;
-      }
+#back-button:not(:hover),
+#back-button:not(:hover) > .toolbarbutton-icon {
+  background: none !important;
+}
 
-      .tabbrowser-tab::before, 
-      .tabbrowser-tab::after{
-        display: none !important;
-      }
+#back-button:hover,
+#back-button:hover > .toolbarbutton-icon {
+  border-radius: 8px !important;
+}
 
-      .tab-close-button {
-        border-radius: 10px !important;
-      }
+.toolbarbutton-icon,
+.toolbarbutton-animatable-box {
+  transition: all .2s !important;
+  background: transparent;
+  border-radius: 6px !important;
+}
 
-      #identity-box,
-      #tracking-protection-icon-container,
-      #urlbar-background {
-         display: none !important;
-      }
+/* Hover Styles */
+#PersonalToolbar .toolbarbutton-1:not([disabled="true"], [checked], [open], :active):hover,
+#tabbrowser-arrowscrollbox:not([scrolledtostart="true"])::part(scrollbutton-up):hover,
+#tabbrowser-arrowscrollbox:not([scrolledtoend="true"])::part(scrollbutton-down):hover,
+toolbarbutton.bookmark-item:not(.subviewbutton, [disabled="true"], [open]):hover,
+.toolbarbutton-1:not([disabled="true"], [checked], [open], :active):hover > .toolbarbutton-icon,
+.toolbarbutton-1:not([disabled="true"], [checked], [open], :active):hover > .toolbarbutton-text {
+  background: var(--primary-grad) !important;
+  border-radius: 8px !important;
+}
 
-      #back-button>.toolbarbutton-icon {
-        transform: scale(.85, .85) !important;
-        animation: none !important;
-        border: none !important;
-        box-shadow: none !important;
-      }
+#PersonalToolbar .toolbarbutton-1:not([disabled="true"]):is([open], [checked], :hover:active),
+#tabbrowser-arrowscrollbox:not([scrolledtostart="true"])::part(scrollbutton-up):hover:active,
+#tabbrowser-arrowscrollbox:not([scrolledtoend="true"])::part(scrollbutton-down):hover:active,
+toolbarbutton.bookmark-item:hover:active:not(.subviewbutton, [disabled="true"]),
+.toolbarbutton-1:not([disabled="true"]):is([open], [checked], :hover:active) > .toolbarbutton-icon {
+  background: var(--dark-bg) !important;
+}
 
-      #back-button:not(:hover),
-      #back-button:not(:hover) > .toolbarbutton-icon {
-        background: none !important;
-      }
+#PanelUI-button {
+  background-color: var(--main-bg) !important;
+  border-image-source: none !important;
+  border-inline-start: 0px !important;
+  margin-inline-start: 0px !important;
+  border-image-slice: 0 !important;
+}
 
-      #back-button:hover,
-      #back-button:hover > .toolbarbutton-icon {
-        border-radius: 8px !important;
-      }
+#urlbar-results {
+  background-color: var(--main-bg) !important;
+  border-radius: 10px !important;
+}
 
-      .toolbarbutton-icon, .toolbarbutton-animatable-box {
-        transition: all .2s !important;
-        background: transparent;
-        border-radius: 6px !important;
-      }
+.urlbarView-body-inner {
+  border-top: none !important;
+}
 
-      #PersonalToolbar .toolbarbutton-1:not([disabled="true"], [checked], [open], :active):hover, #tabbrowser-arrowscrollbox:not([scrolledtostart="true"])::part(scrollbutton-up):hover, #tabbrowser-arrowscrollbox:not([scrolledtoend="true"])::part(scrollbutton-down):hover, toolbarbutton.bookmark-item:not(.subviewbutton, [disabled="true"], [open]):hover, toolbar .toolbarbutton-1:not([disabled="true"], [checked], [open], :active):hover > .toolbarbutton-icon, toolbar .toolbarbutton-1:not([disabled="true"], [checked], [open], :active):hover > .toolbarbutton-text, toolbar .toolbarbutton-1:not([disabled="true"], [checked], [open], :active):hover > .toolbarbutton-badge-stack, #tabs-newtab-button .toolbarbutton-icon {
-        background: var(--primary-grad) !important;
-        border-radius: 8px !important;
-      }
+#nav-bar {
+  border-bottom: 0px !important;
+  box-shadow: none !important;
+}
 
-      #PersonalToolbar .toolbarbutton-1:not([disabled="true"]):is([open], [checked], :hover:active), #tabbrowser-arrowscrollbox:not([scrolledtostart="true"])::part(scrollbutton-up):hover:active, #tabbrowser-arrowscrollbox:not([scrolledtoend="true"])::part(scrollbutton-down):hover:active, toolbarbutton.bookmark-item:hover:active:not(.subviewbutton, [disabled="true"]), toolbarbutton.bookmark-item[open="true"], toolbar .toolbarbutton-1:not([disabled="true"]):is([open], [checked], :hover:active) > .toolbarbutton-icon, toolbar .toolbarbutton-1:not([disabled="true"]):is([open], [checked], :hover:active) > .toolbarbutton-text, toolbar .toolbarbutton-1:not([disabled="true"]):is([open], [checked], :hover:active) > .toolbarbutton-badge-stack {
-        background: var(--dark-bg) !important;
-      }
+#urlbar ::-moz-selection,
+.searchbar-textbox ::-moz-selection {
+  background-color: #dddddd !important;
+  color: #000 !important;
+}
 
-      #back-button {
-        list-style-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAKqNIzIAAAAJcEhZcwAADdcAAA3XAUIom3gAAAAHdElNRQfiAxMSNzVLQoGBAAAAb0lEQVQoz43ROQqDUBSF4UMEESKCBBehOODK7ezSWZnGJmuI4xp+66CX9077FXc40k1o+fCSFRoOOgKLa3Z6QosrNt5EFpesDDwtLlgYSSzOmZlILY758SW7ykO+cYzwWNLjTI9HSc5XS86ypP+6Tw/fVEqhFbEZAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDE4LTAzLTE5VDE4OjU1OjUzKzAxOjAwq3ccegAAACV0RVh0ZGF0ZTptb2RpZnkAMjAxOC0wMy0xOVQxODo1NTo1MyswMTowMNoqpMYAAAAZdEVYdFNvZnR3YXJlAHd3dy5pbmtzY2FwZS5vcmeb7jwaAAAAAElFTkSuQmCC') !important;
-      }
+#urlbar,
+.searchbar-textbox {
+  color: var(--pywal-foreground) !important;
+}
 
-      #forward-button {
-        list-style-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAKqNIzIAAAAJcEhZcwAADdcAAA3XAUIom3gAAAAHdElNRQfiAxMSOA3k2CXQAAAAbklEQVQoz5WRMQqDABAEF4UgKEIIPkKJii+3s7NLpY1N3qBR84axsPYOt1sYuNtdSZLEi5FG1yKk5U9tIQ86dioLiejZKC0k5sPK20JSBn4UFvJkYiG3kIwvM8npAt2Vc8J50onpFOVU7Y51PfcBtUFUSir+geUAAAAldEVYdGRhdGU6Y3JlYXRlADIwMTgtMDMtMTlUMTg6NTY6MTMrMDE6MDDECqmDAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE4LTAzLTE5VDE4OjU2OjEzKzAxOjAwtVcRPwAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAAASUVORK5CYII=') !important;
-      }
+#tabbrowser-tabpanels {
+  background-color: white !important;
+}
+    
 
-      #whats-new-menu-button,
-      #star-button,
-      #pocket-button,
-      #pageActionSeparator,
-      #pageActionButton,
-      #reader-mode-button{
-        display: none !important;
-      }
-
-      #PanelUI-button {
-        background-color: var(--main-bg) !important;
-        border-image-source: none !important;
-        border-inline-start: 0px !important;
-        margin-inline-start: 0px !important;
-        border-image-slice: 0 !important;
-      }
-
-      #urlbar-results {
-        background-color: var(--main-bg) !important;
-        border-radius: 10px !important;
-      }
-
-      .urlbarView-body-inner {
-        border-top: none !important;
-      }
-
-      #nav-bar {
-        border-bottom: 0px !important;
-        box-shadow: none !important;
-      }
-
-      #urlbar ::-moz-selection,
-      .searchbar-textbox ::-moz-selection {
-        background-color: #dddddd !important;
-        color: #000 !important;
-      }
-
-      #urlbar, .searchbar-textbox {
-          color: white !important;
-      }
-
-      #tabbrowser-tabpanels{
-        background-color: #ffffff !important;
-      }
-
-      .tab-loading-burst {
-          display: none !important;
-      }
-    '';
+   '';
   };
 }
